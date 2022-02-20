0606a4d6b5250c4e964a14969117c867db083b09
qubit4 <82866970+qubit4@users.noreply.github.com>
2022-02-20 20:04:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ from encryption import encrypt
 from decryption import decrypt
 
 
-process = input(
-    "Press 1 to generate random passwords, save them to a file, \nand optionally encrypt the file. \nPress 2 to decrypt the file. \n")
+def main():
+    process = input(
+        "Press 1 to generate random passwords, save them to a file, \nand optionally encrypt the file. \nPress 2 to decrypt the file. \n")
 
-if process == "1":
-    while True:
-        generate_password()
-        another = input("Generate another password? Yes or No: ")
-        if another.lower() == "yes":
-            continue
-        encrypt_now = input(
-            "Your passwords are now in 'document1.txt'. Encrypt the file now? Yes or No: ")
-        if encrypt_now.lower() == "yes":
-            encrypt("document1.txt")
-            print(
-                "The file is now encrypted. The encryption key is stored in 'filekey.key'.")
+    if process == "1":
+        while True:
+            generate_password()
+            another = input("Generate another password? Yes or No: ")
+            if another.lower() == "yes":
+                continue
+            encrypt_now = input(
+                "Your passwords are now in 'document1.txt'. Encrypt the file now? Yes or No: ")
+            if encrypt_now.lower() == "yes":
+                encrypt("document1.txt")
+                print(
+                    "The file is now encrypted. The encryption key is stored in 'filekey.key'.")
+                break
             break
-        break
-if process == "2":
-    decrypt("document1.txt")
+    if process == "2":
+        decrypt("document1.txt")
+
+
+if __name__ == "__main__":
+    main()
